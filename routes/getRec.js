ec31e96436ca135bef04e09a93b36da5e4c60492
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2018-10-03 22:50:27 +0530

    Yeah xD

--- routes/getRec.js
+++ routes/getRec.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router= express.Router();
 var mongoose= require('mongoose');
-
+//Lambda expers
 const R = require('ramda');
-
 //MongoDB Stuff
 var cu_ratings= require('../models/cu_ratings');
 
@@ -33,7 +32,13 @@ router.get('/',(req,res)=>{
         matrix=transpose(matrix);
         console.log(matrix);
         matrix=normalizeCosines(matrix,matrix.length,matrix[0].length);
-        console.log(matrix)
+        console.log(matrix);
+        var simSet=keySetMapping(keyset);
+        var rankmatrix= rankItems(matrix);
+        console.log('=================================================');
+        console.log(rankmatrix);
+        console.log('=================================================');
+        printRankList(simSet,rankmatrix,rankmatrix.length);
         res.json(doc);
     });
 
@@ -63,6 +68,77 @@ function normalizeCosines(matrix, rowSize, colSize){
     }
     return matrix;
 }
+/**
+ * Now we shall calculate the similarity 
+ * And the rank items with respect to an item
+ * we are going to use centered cosine similarities
+ * this is also called Pearson's Coefficient
+ */
+function similarity(item1,item2,userLength){
+    var num1=0;
+    var num2=0;
+    for(var i=0; i<userLength; i++){
+        num1+=item1[i];
+        num2+=item2[i];
+    }
+    var num11=0, num22=0;
+    for(var i=0; i<userLength; i++){
+        num11+=(item1[i]*item1[i]);
+        num22+=(item2[i]*item2[i]);
+    }
+    var num= num1*num2;
+    var denum= Math.sqrt(num11)*Math.sqrt(num22);
+    var pearsonCoeff= num/denum;
+    return pearsonCoeff;
+}
+/**
+ * Functional mapping
+ * mapping course ids with numbers for easy calculations
+ */
+function keySetMapping(keys){
+    set={}
+    for(var i=0;i<keys.length; i++){
+        set[i]=keys[i];
+    }
+    return set;
+}
+/**
+ * Function for Item-Item ranking
+ */
+function rankItem(item1,matrix){
+    var ranklist= new Array();
+    for(var i=0; i<matrix.length; i++){
+        if(i!=item1){
+            var similarityX= similarity(matrix[item1],matrix[i],matrix[i].length);
+            ranklist.push({"item":i,"similarity":similarityX});
+        }    
+    }
+    return ranklist.sort((a,b)=>{
+        return b.similarity>a.similarity;
+    });
+}
 
+/**
+ * Ranking all the items by doing above repeatedly
+ */
+function rankItems(matrix){
+    var rankmatrix= new Array(matrix.length);
+    for(var i=0; i<matrix.length; i++){
+        rankmatrix[i]=rankItem(i,matrix);
+    }
+    return rankmatrix;
+}
+/**
+ * Debug function to output item wise ranking
+ */
+function printRankList(keyMap,rankmatrix,rowSize){
+    for(var i=0; i<rowSize; i++){
+        console.log('For ',keyset[i])
+        for(var j=0; j<rowSize-1; j++){
+            console.log("at rank ",j,keyMap[rankmatrix[i][j].item]);
+        }
+        console.log('=============================================');
+    }
+}
 
 module.exports = router;
